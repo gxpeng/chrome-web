484bdb8bba4bb9a7df60e73c90cd5e1efbd3bac3
gxpeng <86676499+gxpeng@users.noreply.github.com>
2023-03-08 03:11:07 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 docker pull tekfik/chrome
-docker run -d --name chrome_web -e VNC_USER=app -e VNC_PASSWORD=password -e VNC_WIDTH=1300 -e VNC_HIGHT=780 -e VNC_TimeZone=America/Los_Angeles -e BROSWER_WIDTH=1280 -e BROSWER_HIGHT=760 --privileged -p 3001:3000 tekfik/chrome
-docker exec -it chrome_web /bin/bash
+docker run -d --name chrome -e VNC_USER=app -e VNC_PASSWORD=password -e VNC_WIDTH=1300 -e VNC_HIGHT=780 -e BROSWER_WIDTH=1280 -e BROSWER_HIGHT=760 -e VNC_TimeZone=America/Los_Angeles --privileged -p 3000:3000 tekfik/chrome
+docker exec -it chrome /bin/bash
   yum -y install google-chrome-stable
   yum -y install sudo unzip
   wget https://codeload.github.com/gxpeng/chrome-web-docker/zip/refs/heads/master
   unzip master
   /bin/cp -f chrome-web-docker-master/system/* /etc/systemd/system/
-  VNC_PORT=3000
+  [ "z""${VNC_USER}" = "z" ]      && VNC_USER=app
+  [ "z""${VNC_PASSWORD}" = "z" ]  && VNC_PASSWORD=password
+  [ "z""${VNC_WIDTH}" = "z" ]     && VNC_WIDTH=1300
+  [ "z""${VNC_HIGHT}" = "z" ]     && VNC_HIGHT=780
+  [ "z""${BROSWER_WIDTH}" = "z" ] && BROSWER_WIDTH=1280
+  [ "z""${BROSWER_HIGHT}" = "z" ] && BROSWER_HIGHT=760
+  [ "z""${VNC_TimeZone}" = "z" ]  && VNC_TimeZone=America/Los_Angeles
+  [ "z""${VNC_PORT}" = "z" ]      && VNC_PORT=3000
   sed -i "s/_USER_/${VNC_USER}/g" `grep -rl "_USER_" /etc/systemd/system/tgvnc.service`
   sed -i "s/_WIDTH_/${VNC_WIDTH}/g" `grep -rl "_WIDTH_" /etc/systemd/system/tgvnc.service`
   sed -i "s/_HIGHT_/${VNC_HIGHT}/g" `grep -rl "_HIGHT_" /etc/systemd/system/tgvnc.service`
